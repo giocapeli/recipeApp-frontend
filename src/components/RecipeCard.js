267a691358dc8b86b9c578637b7ce6fe736dd4d3
giocapeli <capeli.andreas@gmail.com>
2021-11-17 09:50:02 +0100

    css changes

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -3,8 +3,10 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { selectUserOwner } from "../store/user/selectors";
 import RatingCard from "./RatingCard";
+import { useHistory } from "react-router";
 
 export default function RecipeCard(props) {
+  const history = useHistory();
   const { id, title, ratings, imageUrl, matches, highlighted } = props;
   const owner = useSelector(selectUserOwner);
   let isOwner = [];
@@ -18,25 +20,19 @@ export default function RecipeCard(props) {
         highlighted === true && matches.length > 1 ? "glow" : null
       }`}
     >
-      <Link to={`/recipe/${id}`} className="link">
+      <div onClick={() => history.push(`/recipe/${id}`)}>
         <div className="titleContainer">
           <h2>{title}</h2>
         </div>
         <div className="img-container" style={{ height: "300px" }}>
           <img className="img" alt={title} src={imageUrl} />
         </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            flexWrap: "wrap",
-          }}
-        >
+        <div style={{ display: "flex", justifyContent: "center" }}>
           {matches.map((e) => (
             <div className="match">{e}</div>
           ))}
         </div>
-      </Link>
+      </div>
       <div>
         <RatingCard ratings={ratings} id={id} />
       </div>
