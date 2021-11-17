4582b876b810334c15a55d55aab1e8167ffebc15
giocapeli <capeli.andreas@gmail.com>
2021-11-17 09:27:40 +0100

    configurations changes

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const apiUrl =
-  process.env.API_URL || "https://wshouldicook.herokuapp.com/";
+  process.env.API_URL || "https://wshouldicook.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const cloudinaryUrl =
   "https://api.cloudinary.com/v1_1/djpfnjfcl/image/upload";
